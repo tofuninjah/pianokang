b4dffec233c730871e0ccd9b55b916ebef4fba44
Chung Kang <chung.kang@gmail.com>
2016-03-06 11:56:58 -0600

    add about text

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 $(document).foundation();
 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {        // If page is scrolled more than 500px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+
 $('ul.dropdown.menu a').click(function(event) {
 
     event.preventDefault();
